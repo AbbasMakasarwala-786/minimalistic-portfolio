abf0ef06825539ec4d47daba236760c3e9650913
Abbas Saifee Makasarwala <abbasmakasarwala1@gmail.com>
2025-09-25 14:00:19 +0530

    Bento Grid with Grid elements

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,22 +1,25 @@
 import React from 'react'
 import { BentoGrid, BentoGridItem } from './ui/bento-grid'
+import { gridItems } from '@/data'
 
 const Grid = () => {
   return (
-    <section id='about'>
-        <BentoGrid>
-            {[{id:1,title:'Title1',description:'Desc1'},{id:1,title:'Title1',description:'Desc1'},{id:1,title:'Title1',description:'Desc1'}].map
-            ((item) => ( // the item is the array {}  
-                <BentoGridItem 
-                id={item.id}
-                key = {item.id}
-                title={item.title}
-                description = {item.description}
-                />
-            ))
-            
-            }
-        </BentoGrid>
+    <section id="about">
+      <BentoGrid className="w-full py-20">
+        {gridItems.map((item, i) => (
+          <BentoGridItem
+            id={item.id}
+            key={i}
+            title={item.title}
+            description={item.description}
+            className={item.className}
+            img={item.img}
+            imgClassName={item.imgClassName}
+            titleClassName={item.titleClassName}
+            spareImg={item.spareImg}
+          />
+        ))}
+      </BentoGrid>
     </section>
 )
 }
--- components/ui/bento-grid.tsx
+++ components/ui/bento-grid.tsx
@@ -1,5 +1,13 @@
+"use client";
 import { cn } from "@/lib/utils";
-
+import { BackgroundGradientAnimation } from "./background-gradient-animation";
+// import { Globe } from "./Globe";
+import GridGlobe from "./GridGlobe";
+import { useState } from "react";
+import animationData from '@/data/confetti.json'
+import Lottie from "react-lottie";
+import MagicButton from "./MagicButton";
+import { IoCopyOutline } from "react-icons/io5";
 export const BentoGrid = ({
   className,
   children,
@@ -10,8 +18,9 @@ export const BentoGrid = ({
   return (
     <div
       className={cn(
-        "mx-auto grid max-w-7xl grid-cols-1 gap-4 md:auto-rows-[18rem] md:grid-cols-3",
-        className,
+        // change gap-4 to gap-8, change grid-cols-3 to grid-cols-5, remove md:auto-rows-[18rem], add responsive code
+        "grid grid-cols-1 md:grid-cols-6 lg:grid-cols-5 md:grid-row-7 gap-4 lg:gap-8 mx-auto",
+        className
       )}
     >
       {children}
@@ -19,38 +28,148 @@ export const BentoGrid = ({
   );
 };
 
+
 export const BentoGridItem = ({
   className,
   title,
   description,
-  header,
-  icon,
-  id:number
+  id,
+  img,
+  imgClassName,
+  titleClassName,
+  spareImg,
 }: {
   className?: string;
   title?: string | React.ReactNode;
   description?: string | React.ReactNode;
   header?: React.ReactNode;
   icon?: React.ReactNode;
-  id:number
+  id: number;
+  img?: string;
+  imgClassName?: string;
+  titleClassName?: string;
+  spareImg?: string;
 }) => {
+  
+  const [copied,setCopied] = useState(false);
+  const handleCopy=()=>
+  {
+    navigator.clipboard.writeText("abbasmakasarwala1@gmail.com")
+    setCopied(true);
+  }
+  const leftLists = ["Machine Learning","Deep Learning","MLOPs"];
+  const rightLists = ["AWS","Python","JavaScript"];
   return (
     <div
       className={cn(
-        "group/bento shadow-input row-span-1 flex flex-col justify-between space-y-4 relative rounded-3xl border border-neutral-200 bg-white p-4 transition duration-200 hover:shadow-xl dark:border-white/[0.2] dark:bg-black dark:shadow-none border-red-500",
-        className,
+        // remove p-4 rounded-3xl dark:bg-black dark:border-white/[0.2] bg-white  border border-transparent, add border border-white/[0.1] overflow-hidden relative
+        "row-span-1 relative overflow-hidden rounded-3xl border border-white/[0.1] group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4",
+        className
       )}
+      style={{
+        //   add these two
+        //   you can generate the color from here https://cssgradient.io/
+        background: "rgb(4,7,29)",
+        backgroundColor:
+          "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
+      }}
     >
-      {header}
-      <div className="transition duration-200 group-hover/bento:translate-x-2">
-        {icon}
-        <div className="mt-2 mb-2 font-sans font-bold text-neutral-600 dark:text-neutral-200">
-          {title}
+
+      <div className={`${id === 6 && 'flex justify-center'} h-full`}>
+        <div className="w-full h-full absolute">
+          {img && (
+            <img
+              src={img}
+              alt={img}
+              className={cn(imgClassName, "object-cover", 'object-center')
+              }
+            />
+          )}
         </div>
-        <div className="font-sans text-xs font-normal text-neutral-600 dark:text-neutral-300">
-          {description}
+        <div className={`absolute right-0 -bottom-5 ${id === 5 && 'w-full opacity-80'}`}>
+          {spareImg && (
+            <img
+              src={spareImg}
+              alt={spareImg}
+              className={"object-cover,object-center,w-full h-full"}
+            />
+          )}
+        </div>
+        {id === 6 && (
+          <BackgroundGradientAnimation>
+            {/* <div className="absolute z-50 flex
+                item-center justify-center text-white font-bold" /> */}
+          </BackgroundGradientAnimation>
+        )}
+        <div className={cn(
+          titleClassName, 'group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flwx flex-col px-5 p-5 lg:p-10'
+        )}>
+          <div className="font-sans font-extralight text-[#c1c3d3] text-sm md:text-xs lg:text-base z-10">
+            {description}
+          </div>
+          <div className="font-sans font-bold text-lg lg:text-3xl max-w-96 z-10">
+            {title}
+          </div>
+
+          {id === 2 && <GridGlobe />}
+          {id === 3 && (
+            <div className="flex gap-1 lg:gap-3 w-max absolute right-3 lg:right-2 -mt-10 lg:-mt-20">
+              {/* tech stack lists */}
+              <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
+                {leftLists.map((item, i) => (
+                  <span
+                    key={i}
+                    className="lg:py-2 lg:px-1 py-2 px-3 text-xs  opacity-50 
+                    lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                  >
+                    {item}
+                  </span>
+                ))}
+                <span className="lg:py-4 lg:px-3 py-4 px-3  rounded-lg text-center bg-[#10132E]"></span>
+              </div>
+              <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
+                {/* <span className="lg:py-4 lg:px-3 py-4 px-3  rounded-lg text-center bg-[#10132E]"></span> */}
+                {rightLists.map((item, i) => (
+                  <span
+                    key={i}
+                    className="lg:py-2 lg:px-1 py-2 px-3 text-xs opacity-50 
+                    lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                  >
+                    {item}
+                  </span>
+                ))}
+              </div>
+            </div>
+          )}
+          {id === 6 && (
+            <div className="mt-5 relative">
+              <div className={`absolute -bottom-5 right-0`}>
+                  <Lottie options={{
+                    loop:copied,
+                    autoplay:copied,
+                    animationData:animationData,
+                    rendererSettings:{
+                        preverveAspectRatio:'xMidYMid slice'
+                    }
+                  }} />
+
+              </div>
+              <MagicButton
+              title={copied ? "Email Copied" : 'Copy My Email'}
+              icon ={<IoCopyOutline />}
+              position="left"
+              otherClasses="!bg-[#161a31]"
+              handleClick={handleCopy}
+              >
+
+              </MagicButton>
+            </div>
+          )
+          }
         </div>
       </div>
+
     </div>
+
   );
 };
