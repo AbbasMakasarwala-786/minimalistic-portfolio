4fd907eaca50f9e4854d042ea91d67d4170dab7c
Abbas Saifee Makasarwala <abbasmakasarwala1@gmail.com>
2025-10-04 19:18:06 +0530

    Added html file for google search

--- next.config.ts
+++ next.config.ts
@@ -2,17 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output:'export',
-  typescript:{
-    ignoreBuildErrors:true,
+  output: 'export',
+  typescript: {
+    ignoreBuildErrors: true,
   },
-  eslint:{
-    ignoreDuringBuilds:true,
-  }
-};
-// next.config.js
-module.exports = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   trailingSlash: false,
   skipTrailingSlashRedirect: true,
-}
+};
+
 export default nextConfig;
